94fe4b2f266cfa2e9e847fe0e9bf38065a2772fa
aluno07 sala04 <pricila.kovalski@gmail.com>
2019-03-26 11:17:40 -0300

    atualização em sala

--- Locadora-Cassia/Locadora/src/br/com/targettrust/locadora/entidades/Equipamento.java
+++ Locadora-Cassia/Locadora/src/br/com/targettrust/locadora/entidades/Equipamento.java
@@ -2,14 +2,16 @@ package br.com.targettrust.locadora.entidades;
 
 public class Equipamento {
 	
-	private String nome;
+	private String descricao;
 
-	public String getNome() {
-		return nome;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
+
+	
 
 }
